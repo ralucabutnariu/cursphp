299568662a667a46e1afa194914dafd23bbf41bb
raluca.butnariu <raluca.butnariu@emag.ro>
2016-11-02 18:15:15 +0200

    Ex 3.4

---
+++ ex3.4.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+  Write a function compare(x, y), x and y integer, that compares x and y:
+    If x < y writes “less”
+    If x = y writes “equal”
+    If x > y writes “greater”
+ */
+
+/**
+ * @param $a
+ * @param $b
+ * @return string
+ */
+function compare($a, $b)
+{
+    if ($a < $b) {
+        return 'less than';
+    } elseif($a == $b) {
+        return 'equal to';
+    }
+    return 'greater than';
+}
+
+$x = 1;
+$y = 2;
+echo $x." is ".compare($x, $y)." ".$y;
+echo "\n";
+
+$x = 1;
+$y = 1;
+echo $x." is ".compare($x, $y)." ".$y;
+echo "\n";
+
+$x = 2;
+$y = 1.5;
+echo $x." is ".compare($x, $y)." ".$y;
+echo "\n";
+?>
+
+
